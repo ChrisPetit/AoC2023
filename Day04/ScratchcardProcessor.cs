79d06ec0b0ce662c808d78f994c626ac69637e8a
Chris Petit <15943525+ChrisPetit@users.noreply.github.com>
2023-12-13 20:35:44 +0100

    Refactor ScratchcardProcessor class for clarity
    Improved the code readability in the ScratchcardProcessor class. Variable names have been made more descriptive and the function responsible for enqueueing of cards based on matches has been extracted into a separate private method, enhancing maintainability and understanding of the code.

--- Day04/ScratchcardProcessor.cs
+++ Day04/ScratchcardProcessor.cs
@@ -1,37 +1,46 @@
+using System.Collections;
+
 namespace Day04;
 public static class ScratchcardProcessor
 {
     public static int ProcessCards(List<(List<int> winningNumbers, List<int> playerNumbers)> cards)
     {
-        var totalCards = 0;
-        var toProcess = new Queue<int>(); // Queue for cards to be processed
+        var processedCardsCount = 0;
+        var cardsToProcess = new Queue<int>(); // Queue for cards to be processed
 
         // Initial queueing of all cards
         for (var i = 0; i < cards.Count; i++)
         {
-            toProcess.Enqueue(i);
+            cardsToProcess.Enqueue(i);
         }
 
-        while (toProcess.Count > 0)
+        while (cardsToProcess.Count > 0)
         {
-            var currentIndex = toProcess.Dequeue();
-            totalCards++;
+            var currentIndex = cardsToProcess.Dequeue();
+            processedCardsCount++;
 
             var currentCard = cards[currentIndex];
-            var matches = currentCard.winningNumbers.Intersect(currentCard.playerNumbers).Count();
-
+            var matches = currentCard.winningNumbers.Count(x => currentCard.playerNumbers.Contains(x));
+            
             // Queue additional cards based on matches
-            for (var j = 1; j <= matches; j++)
-            {
-                var nextIndex = currentIndex + j;
-                if (nextIndex < cards.Count)
-                {
-                    toProcess.Enqueue(nextIndex);
-                }
-            }
+            EnqueueBasedOnMatches(cards, currentIndex, matches, cardsToProcess);
         }
 
-        return totalCards;
+        return processedCardsCount;
+    }
+    
+    private static void EnqueueBasedOnMatches(ICollection cards, int currentIndex, 
+        int matches, Queue<int> cardsToProcess)
+    {
+        // this new function handles enqueueing of cards based on the number of matches
+        for (var j = 1; j <= matches; j++)
+        {
+            var nextIndex = currentIndex + j;
+            if (nextIndex < cards.Count)
+            {
+                cardsToProcess.Enqueue(nextIndex);
+            }
+        }
     }
 }
 
